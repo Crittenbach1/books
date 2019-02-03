ea1632e63cfe39274c6832aa8daec9018e5ec3c7
crittenbach1 <crittenbach@gmail.com>
2019-02-03 01:10:54 -0500

    add book at specified size

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,7 +6,12 @@ class Book
   field :author, type: String
   field :owned, type: Mongoid::Boolean
 
-  has_mongoid_attached_file :image
+
+
+  has_mongoid_attached_file :image,
+                                  :styles => {
+                                              :original  => "434x271>"
+                                             }
 
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
